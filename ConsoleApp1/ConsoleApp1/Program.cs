135ebf7484d60f4d4b1261b87e3510931f900c07
LAPTOP-DI30GGAL\iiisinnnn <watasin.kongkhun@mail.kmutt.ac.th>
2021-03-28 15:13:34 +0700

    คำตอบข้อที่.3

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t1 = 0, t2 = 0, t3 = 0;
+
+
+            Console.Write("Input Tank1 : ");
+
+
+            t1 = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Input Tank2 : ");
+
+
+            t2 = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Input Tank3 ; ");
+
+
+            t3 = int.Parse(Console.ReadLine());
+
+
+            int A = 0, B = 0;
+
+            //ไม่ไหวแล้ว ขอลาไปอ้วกหน่อยครับ TT
+        }
+    }
+}
